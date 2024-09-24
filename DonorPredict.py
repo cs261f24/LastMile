24d1644530640e88213bbcd4bf3215cf644aed4e
BrianMatton <156353911+BrianMatton@users.noreply.github.com>
2024-09-24 15:17:24 -0400

    Add files via upload

---
+++ DonorPredict.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read the Excel file into a DataFrame
+filePath = r'C:\Users\matto\Documents\LastMile\LastMile.xlsx'
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel(filePath)
+
+# Display the DataFrame
+print(df)

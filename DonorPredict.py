8ecd789eb6d5d1ebb8ae3991275b855c029fbbe8
BrianMatton <156353911+BrianMatton@users.noreply.github.com>
2024-09-24 15:19:52 -0400

    Update DonorPredict.py

--- DonorPredict.py
+++ DonorPredict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Excel file into a DataFrame
-filePath = r'C:\Users\matto\Documents\LastMile\LastMile.xlsx'
+filePath = /LastMile/TestExcel.xlsx
 
 # Read the Excel file into a DataFrame
 df = pd.read_excel(filePath)

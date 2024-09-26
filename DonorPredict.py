cddd6d10aff0ab5975eb0653adfb81c5026fe3d2
BrianMatton <156353911+BrianMatton@users.noreply.github.com>
2024-09-26 15:37:24 -0400

    Update DonorPredict.py
    Added functionality that allows for reading on specific dates to create separate data frames (Unfinished)

--- DonorPredict.py
+++ DonorPredict.py
@@ -6,5 +6,17 @@ filePath = /LastMile/TestExcel.xlsx
 # Read the Excel file into a DataFrame
 df = pd.read_excel(filePath)
 
+#Reads in the date column to a DataFrame
+dateDF = pd.read_excel(filePath, usecols=['AI'])
+
+#Renames the column from AI to Date
+df.rename(columns={'AI': 'Date'}, inplace = True)
+
+# Converts the Date DataFrame to datetime
+df['Date'] = pd.to_datetime(df['Date'])
+
+#Defines the specific date to filter by
+specificDate = pd.to_datetime('2024-05-15')
+
 # Display the DataFrame
-print(df)
+print(dateDF)
